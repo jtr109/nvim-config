f19342429671a44cb11f4e20bc0a9e7d73735a22
Ryan Li <conbas2019@gmail.com>
2022-11-26 22:39:51 +0800

    feat: add vim global in lua

--- lua/lsp/lua.lua
+++ lua/lsp/lua.lua
@@ -1,1 +1,10 @@
-require('lspconfig')['sumneko_lua'].setup{}
+require('lspconfig')['sumneko_lua'].setup{
+	settings = {
+		Lua = {
+			diagnostics = {
+				-- Let the language server to recognize the `vim` global
+				globals = { 'vim' },
+			}
+		}
+	}
+}
