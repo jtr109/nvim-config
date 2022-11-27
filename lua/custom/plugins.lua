3a7476872e31c075fac86493b1f3f3e699d83019
Ryan Li <conbas2019@gmail.com>
2022-11-27 21:01:45 +0800

    feat: show packer message in popup

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,4 +1,19 @@
-return require('packer').startup(function(use)
+-- Use a protected call to make sure packer installed
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+	return
+end
+
+-- Show Packer status in a popup window
+packer.init {
+	display = {
+		open_fn = function()
+			return require("packer.util").float { border = "rounded" }
+		end,
+	},
+}
+
+return packer.startup(function(use)
 	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
 
