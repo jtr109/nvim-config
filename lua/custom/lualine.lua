7499a0b0f761b3451e3887cc55743eab1263e1df
Ryan Li <conbas2019@gmail.com>
2022-12-03 21:02:59 +0800

    feat: format filename

--- lua/custom/lualine.lua
+++ lua/custom/lualine.lua
@@ -2,6 +2,20 @@
 
 local status_ok, lualine = pcall(require, "lualine")
 if not status_ok then
-	return
+  return
 end
-lualine.setup()
+lualine.setup({
+  sections = {
+    lualine_c = {
+      {
+        'filename',
+        -- Filename path formatting
+        -- 0: Just the filename
+        -- 1: Relative path
+        -- 2: Absolute path
+        -- 3: Absolute path, with tilde as the home directory
+        path = 1,
+      }
+    }
+  }
+})
