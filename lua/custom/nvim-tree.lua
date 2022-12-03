35faeafbc1359dcf66d75e84a848dcd8e171f106
Ryan Li <conbas2019@gmail.com>
2022-12-03 08:11:59 +0800

    feat: format

--- lua/custom/nvim-tree.lua
+++ lua/custom/nvim-tree.lua
@@ -1,28 +1,35 @@
 local status_ok, tree = pcall(require, "nvim-tree")
 if not status_ok then
-	return
+    return
 end
-tree.setup()
+tree.setup({
+    view = {
+        mappings = {
+            list = {
+                { key = "", action = "" }
+            }
+        }
+    }
+})
 
 -- Auto close nvim when nvim-tree is the last window.
 -- Copy from: https://github.com/nvim-tree/nvim-tree.lua/wiki/Auto-Close
 -- nvim-tree is also there in modified buffers so this function filter it out
 local modifiedBufs = function(bufs)
     local t = 0
-    for k,v in pairs(bufs) do
+    for _, v in pairs(bufs) do
         if v.name:match("NvimTree_") == nil then
             t = t + 1
         end
     end
     return t
 end
-
 vim.api.nvim_create_autocmd("BufEnter", {
     nested = true,
     callback = function()
         if #vim.api.nvim_list_wins() == 1 and
-        vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil and
-        modifiedBufs(vim.fn.getbufinfo({bufmodified = 1})) == 0 then
+            vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil and
+            modifiedBufs(vim.fn.getbufinfo({ bufmodified = 1 })) == 0 then
             vim.cmd "quit"
         end
     end
