3e7a2dc3ac8ff8943b715a896087c71a655a3430
Ryan Li <conbas2019@gmail.com>
2022-12-08 15:05:42 +0800

    fix camel case not working

---
+++ after/ftplugin/kusto.lua
@@ -0,0 +1,4 @@
+-- vim.opt.tabstop = 4 -- set the width of a tab in spaces count
+-- vim.opt.shiftwidth = 4 -- the width of indent
+-- vim.opt.expandtab = true -- insert spaces when hit <Tab> or indent
+vim.opt_local.spelloptions = 'camel' -- Fix CamelCase not working
