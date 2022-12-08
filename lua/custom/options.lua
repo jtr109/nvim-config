900071f5547ef1f4d6e1651f000bf830ca088f2a
Ryan Li <conbas2019@gmail.com>
2022-12-08 13:31:50 +0800

    feat: disable tabrw

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -1,8 +1,8 @@
 -- disable netrw at the very start of your init.lua (strongly advised)
 -- dependencies:
 --   - nvim-tree.lua
--- vim.g.loaded_netrw = 1
--- vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
 -- :help options
 local options = {
